370dfbdbcb997879b34882a5dffe35612327bfef
HanJeongmok <wjdahe1845@gmail.com>
2026-02-13 14:49:10 +0900

    wiseSaying 을 post클래스로 변경

--- src/main/java/com/back/domain/wiseSaying/entity/Post.java
+++ src/main/java/com/back/domain/wiseSaying/entity/Post.java
@@ -3,13 +3,13 @@ package com.back.domain.wiseSaying.entity;
 import jakarta.persistence.*;
 
 @Entity
-public class WiseSaying {
+public class Post {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     @Column(length=100,nullable =false)
-    private  String content;
-    @Column(length=50, nullable = false)
-    private String author;
+    private  String title;
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
 }
