3ff18ef8310986166a795b324f2fde4de0af951e
HanJeongmok <wjdahe1845@gmail.com>
2026-02-23 11:30:52 +0900

    service구현

---
+++ src/main/java/com/back/domain/post/entity/Post.java
@@ -0,0 +1,28 @@
+package com.back.domain.post.entity;
+
+import jakarta.persistence.*;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  int id=0;
+    @Column(length=100,nullable =false)
+    private  String title;
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private  String content;
+    private LocalDateTime createDate;
+    private LocalDateTime modifyDate;
+
+    public Post(String title, String content){
+        this.createDate = LocalDateTime.now();
+        this.modifyDate = LocalDateTime.now();
+        this.title = title;
+        this.content = content;
+    }
+}
