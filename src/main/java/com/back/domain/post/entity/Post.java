8531941d1b347f2b006733c3a56fe80f4fa76ba2
HanJeongmok <wjdahe1845@gmail.com>
2026-02-23 15:48:39 +0900

    35강 베이스 엔티티만들고 상속받기

--- src/main/java/com/back/domain/post/entity/Post.java
+++ src/main/java/com/back/domain/post/entity/Post.java
@@ -1,37 +1,24 @@
 package com.back.domain.post.entity;
 
-import jakarta.persistence.*;
+import com.back.global.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
-import java.time.LocalDateTime;
 
 @Entity
 @NoArgsConstructor
 @Getter
 @Setter
-@EntityListeners(AuditingEntityListener.class)
-public class Post {
+public class Post extends BaseEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private  int id=0;
-    @Column(length=100,nullable =false)
-    private  String title;
+    @Column(length = 100, nullable = false)
+    private String title;
     @Column(columnDefinition = "TEXT", nullable = false)
-    private  String content;
-    @CreatedDate
-    private LocalDateTime createDate;
-    @LastModifiedDate
-    private LocalDateTime modifyDate;
+    private String content;
 
-    public Post(String title, String content){
-        this.createDate = LocalDateTime.now();
-        this.modifyDate = LocalDateTime.now();
+    public Post(String title, String content) {
         this.title = title;
         this.content = content;
     }
