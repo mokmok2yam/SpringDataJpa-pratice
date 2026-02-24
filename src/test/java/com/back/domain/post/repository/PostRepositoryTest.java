1c961f2a41304cee34380ad634264d2e8c38e511
HanJeongmok <wjdahe1845@gmail.com>
2026-02-24 11:10:14 +0900

    37,48,39,40t2,t추가하고 롤백및 트랜잭션 설정

--- src/test/java/com/back/domain/post/repository/PostRepositoryTest.java
+++ src/test/java/com/back/domain/post/repository/PostRepositoryTest.java
@@ -4,6 +4,8 @@ import com.back.domain.post.entity.Post;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -11,9 +13,30 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class PostRepositoryTest {
     @Autowired
     private PostRepository postRepository;
+
     @Test
+    @Transactional
+    @Rollback
     void t1(){
         Post post = postRepository.findById(2).get();
         assertThat("제목2").isEqualTo(post.getTitle());
     }
+    @Test
+    @Transactional
+    @Rollback
+    void t2(){
+        Post post = new Post("제목3","내용3");
+        Post savedPost = postRepository.save(post);
+        assertThat(savedPost.getId()).isNotNull();
+        assertThat(savedPost.getTitle()).isEqualTo("제목3");
+        assertThat(savedPost.getContent()).isEqualTo("내용3");
+    }
+    @Test
+    @Transactional
+    @Rollback
+    void t3(){
+        long cnt =postRepository.count();
+        assertThat(cnt).isEqualTo(2);
+    }
+
 }
